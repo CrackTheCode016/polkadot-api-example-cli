13c2764d3bb223da74f664a7a2c448ab60b51217
Bader Youssef <ibnbassem@gmail.com>
2025-02-03 15:47:21 -0500

    basic account watcher

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,52 @@
 // `dot` is the name we gave to `npx papi add`
-import { assethub, custom } from "@polkadot-api/descriptors"
+import { wnd } from "@polkadot-api/descriptors"
 import { createClient, PolkadotClient } from "polkadot-api"
 import { getSmProvider } from "polkadot-api/sm-provider";
-import { getWsProvider } from "polkadot-api/ws-provider/node";
-import { chainSpec } from "polkadot-api/chains/polkadot_asset_hub";
-import { chainSpec as relayChainSpec } from "polkadot-api/chains/polkadot";
+import { chainSpec as westEndChainSpec } from "polkadot-api/chains/westend2";
 import { start } from "polkadot-api/smoldot";
-import { withPolkadotSdkCompat } from "polkadot-api/polkadot-sdk-compat";
+import figlet from "figlet";
+import { Command } from "commander";
+import chalk from "chalk";
 
 async function withLightClient(): Promise<PolkadotClient> {
     // Start the light client
     const smoldot = start();
     // The Polkadot Relay Chain
-    const relayChain = await smoldot.addChain({ chainSpec: relayChainSpec })
-    // Polkadot AssetHub - we include the relay chain as a "potential relay chain", as this is part of smoldot to check the chainspec for what relay chain a parachain uses
-    const assetHub = await smoldot.addChain({ chainSpec, potentialRelayChains: [relayChain] });
-    // Combine it and make a client!
+    const relayChain = await smoldot.addChain({ chainSpec: westEndChainSpec })
     return createClient(
-        getSmProvider(assetHub)
-    );
-}
-async function withWebSocket(url: string): Promise<PolkadotClient> {
-    return createClient(
-        // Polkadot-SDK Nodes have issues, we recommend adding this enhancer
-        // see Requirements page for more info
-        withPolkadotSdkCompat(
-            getWsProvider("ws://localhost:9944")
-        )
+        getSmProvider(relayChain)
     );
 }
 
+chalk
 async function main() {
-    // For custom chain options, uncomment the following and add your custom URL (such as: localhost). You may also comment out the above client in this case.
-    const wsClient = await withWebSocket("ws://localhost:9944");
-    const lightClient = await withLightClient();
+    const program = new Command();
 
-    // To interact with the chain, you need to get the `TypedApi`, which includes
-    // all the types for every call in that chain:
+    console.log(chalk.white.dim(figlet.textSync('Polkadot Account Watcher')))
 
-    // Note, if you want to use a custom chain, you must change it to the name of the metadata from PAPI (see README for more details).
-    // You must also use the "wsClient" instead.
-    const dotApi = lightClient.getTypedApi(assethub);
-    // For example, with a custom chain:
-    // const dotApi = wsClient.getTypedApi(custom);
+    program.version('0.0.1').description('Polkadot Account Watcher - A simple CLI tool to watch account balance on Polkadot network')
+        .option('-a, --account <account>', 'Account to watch')
+        .parse(process.argv);
 
-    // Then, you can make calls like this...
-    const last_runtime_upgrade = await dotApi.query.System.LastRuntimeUpgrade.getValue();
-    console.log(last_runtime_upgrade)
+    const options = program.opts();
+
+    if (options.account) {
+        console.log(chalk.black.bgRed("Watching account:"), chalk.bold.whiteBright(options.account));
+        const lightClient = await withLightClient();
+        const dotApi = lightClient.getTypedApi(wnd);
+        dotApi.event.Balances.Transfer.watch().subscribe((event) => {
+            const { from, to, amount } = event.payload;
+            if (from.toString() == options.account || to.toString() == options.account) {
+                console.log(chalk.black.bgRed(`Transfer occurred!`));
+                console.log(chalk.black.bgCyan("From:"), chalk.bold.whiteBright(from.toString()));
+                console.log(chalk.black.bgBlue("To:"), chalk.bold.whiteBright(to.toString()));
+                console.log(chalk.black.bgGreen("Amount:"), chalk.bold.whiteBright(amount.toString()));
+            }
+        });
+    } else {
+        console.error('Account is required');
+        return;
+    }
 }
 
 main()
