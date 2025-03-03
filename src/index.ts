fba278e92d589b1d82b7876dfbb8c6f63a253710
Bader Youssef <ibnbassem@gmail.com>
2025-03-03 13:33:11 -0500

    empty template

--- src/index.ts
+++ src/index.ts
@@ -22,34 +22,7 @@ async function withLightClient(): Promise<PolkadotClient> {
 }
 
 async function main() {
-    const program = new Command();
-    console.log(chalk.white.dim(figlet.textSync("Web3 Mail Watcher")))
-
-    program.version('0.0.1').description('Web3 Mail Watcher - A simple CLI tool to watch for remarks on Polkadot network')
-        .option('-a, --account <account>', 'Account to watch')
-        .parse(process.argv);
-
-    const options = program.opts();
-
-    if (options.account) {
-        console.log(chalk.black.bgRed("Watching account:"), chalk.bold.whiteBright(options.account));
-        const lightClient = await withLightClient();
-        const dotApi = lightClient.getTypedApi(wnd);
-        dotApi.event.System.Remarked.watch().subscribe((event) => {
-            // We look for a specific hash, indicating that its our address + an email
-            const { sender, hash } = event.payload;
-            const calculatedHash = bytesToHex(blake2b(`${options.account}+email`, { dkLen: 32 }));
-            if (`0x${calculatedHash}` == hash.asHex()) {
-                sound.play("youve-got-mail-sound.mp3")
-                console.log(chalk.black.bgRed(`You got mail!`));
-                console.log(chalk.black.bgCyan("From:"), chalk.bold.whiteBright(sender.toString()));
-                console.log(chalk.black.bgBlue("Hash:"), chalk.bold.whiteBright(hash.asHex()));
-            }
-        });
-    } else {
-        console.error('Account is required');
-        return;
-    }
+    // CLI Code goes here...
 }
 
 main()
